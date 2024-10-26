51c446199c97a5e42c20a49f5b2b327547f6de17
Keith Stevens <fozziethebeat@gmail.com>
2024-10-26 11:55:09 -0700

    Fancying up the batch page

--- frontend/src/app/batch/[id]/page.tsx
+++ frontend/src/app/batch/[id]/page.tsx
@@ -18,6 +18,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import { BatchForm } from "@/components/BatchForm";
 
 const QUERY = gql`
@@ -93,6 +94,73 @@ export default async function BatchPage({ params }) {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+
+      <div className="space-y-1">
+        <h4 className="text-xl font-medium leading-none">
+          Batch Recipe Details
+        </h4>
+      </div>
+      <Separator className="my-1" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <Card>
+          <CardHeader className="px-7">
+            <CardTitle>Base Oils</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-8">
+              {batch.recipe.baseOils.map((oil) => (
+                <div key={oil.ingredient.id} className="flex items-center">
+                  <div className="ml-4 space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {oil.ingredient.name}
+                    </p>
+                  </div>
+                  <div className="ml-auto space-y-1">
+                    <div className="ml-auto font-medium">
+                      {((oil.quantity / 100) * batch.amount).toFixed(2)}g
+                    </div>
+                    <p className="text-sm text-muted-foreground">
+                      {oil.quantity}%
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="px-7">
+            <CardTitle>Essential Oils</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-8">
+              {batch.recipe.essentialOils.map((oil) => (
+                <div key={oil.ingredient.id} className="flex items-center">
+                  <div className="ml-4 space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {oil.ingredient.name}
+                    </p>
+                  </div>
+                  <div className="ml-auto space-y-1">
+                    <div className="ml-auto font-medium">
+                      {((oil.quantity / 100) * batch.amount).toFixed(2)}g
+                    </div>
+                    <p className="text-sm text-muted-foreground">
+                      {oil.quantity}%
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="space-y-1">
+        <h4 className="text-xl font-medium leading-none">Batch Soap labels</h4>
+      </div>
+      <Separator className="my-1" />
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {batch.batchSoapLabel.map((label, idx) => (
           <Card key={label.id} className="w-[360px]">
